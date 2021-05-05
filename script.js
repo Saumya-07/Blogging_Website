9cf4f87a36cdfb20698120b3bde66d5566d64764
Saumya Gupta <saumyagupta720@gmail.com>
2021-05-05 23:54:00 +0530

    counter api

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ close.addEventListener('click',() => {
     navHorizontal.classList.add('hide');
     navHorizontal.classList.remove('show');
 })
+
+const views=document.getElementById('views');
+const users=document.getElementById('users');
+
+//update views count
+const updateViews = () =>{
+    fetch('https://api.countapi.xyz/update/blogging/views?amount=1')
+    .then(res=>res.json())
+    .then(res=>{
+        views.innerHTML=res.value;
+    })
+}
+
+updateViews();
+
+//update users count
+const updateUsers = () =>{
+    fetch('https://api.countapi.xyz/update/blogging/views?amount=-9')
+    .then(res=>res.json())
+    .then(res=>{
+        users.innerHTML=res.value;
+    })
+}
+
+updateUsers();
